c2cb2ff4c8d33b1bb5567bd2e2a30dd3a1730ca1
ToseebNadaf <toseebnadaf@gmail.com>
2025-02-06 15:17:59 +0530

    Database connected

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import app from "./app.js";
+import connectDB from "./config/db.js";
+
+connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on: ${process.env.PORT}`);
